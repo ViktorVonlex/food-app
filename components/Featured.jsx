641837ef99707d6943df3b694d3f6f34805306ce
ViktorVonlex <suster.ji@gmail.com>
2022-08-17 22:00:52 +0200

    Featured at homepage with random images working, empty pizza list.

--- components/Featured.jsx
+++ components/Featured.jsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Image from 'next/image'
 import styles from '../styles/Featured.module.css'
 
 function Featured() {
+    const [sliderIndex, setSliderIndex] = useState(0)
+
+    const handleArrow = (direction) => {
+        if(direction==='l'){
+            setSliderIndex(sliderIndex !== 0 ? sliderIndex-1 : 2)
+        }
+        if(direction==='r'){
+            setSliderIndex(sliderIndex !== 2 ? sliderIndex+1 : 0)
+        }
+    }
+
     const images = [
         "/img/featured1.png",
         "/img/featured2.png",
@@ -12,15 +23,19 @@ function Featured() {
 
     return (
         <div className={styles.container}>
-            <Image src="/img/arrowl.png" alt="" layout='fill' />
-            <div className={styles.wrapper}></div>
-            <div className={styles.imgContainer}>
-                {images.map((img, i) =>
-                    <Image src="/img/featured.png" key={i} alt="" layout='fill' />
-                )}
-
+            <div className={styles.wrapper} style={{transform:`translateX(${-100*sliderIndex}vw)`}}>
+                {images.map((img, i) => (
+                    <div className={styles.imgContainer} key={i}>
+                        <Image src={img} alt="" layout='fill' objectFit='contain'/>
+                    </div>
+                ))}
             </div>
-            <Image src="/img/arrowr.png" alt="" layout='fill' />
+            <div className={styles.arrowContainer} style={{right:0}} onClick={()=>handleArrow("r")}>
+                <Image src="/img/arrowr.png" alt="" layout='fill' />
+            </div>
+            <div className={styles.arrowContainer} style={{left:0}} onClick={()=>handleArrow("l")}>
+                <Image src="/img/arrowl.png" alt="" layout='fill' />
+            </div>
         </div>
     )
 }
