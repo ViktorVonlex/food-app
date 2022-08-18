0c37a0d2019a2bf92c281f80f6d2904f25ecd619
ViktorVonlex <suster.ji@gmail.com>
2022-08-18 16:12:49 +0200

    Product listing
    List of products on the main page with static price and description. Fixed navbar scrolling.

---
+++ components/PizzaList.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import styles from '../styles/PizzaList.module.css'
+import PizzaCard from './PizzaCard'
+
+function PizzaList() {
+  return (
+    <div className={styles.container}>
+        <h1 className={styles.title}>BEST PIZZA IN TOWN!</h1>
+        <p className={styles.desc}>
+        Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Ut enim ad minim veniam, quis nostrud
+        exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Nullam dapibus fermentum ipsum.
+        Nulla non lectus sed nisl molestie malesuada.
+        </p>
+        <div className={styles.wrapper}>
+            <PizzaCard></PizzaCard>
+            <PizzaCard></PizzaCard>
+            <PizzaCard></PizzaCard>
+            <PizzaCard></PizzaCard>
+            <PizzaCard></PizzaCard>
+            <PizzaCard></PizzaCard>
+            <PizzaCard></PizzaCard>
+            <PizzaCard></PizzaCard>
+            <PizzaCard></PizzaCard>
+        </div>
+    </div>
+  )
+}
+
+export default PizzaList
