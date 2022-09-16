9150461afcc0c8c5213d4565af7978b779ba8824
ViktorVonlex <suster.ji@gmail.com>
2022-09-16 13:26:15 +0200

    Post for product
    SHOULD be working but it doesn't at work

--- models/Product.js
+++ models/Product.js
@@ -1,30 +1,36 @@
-import mongoose from "mongoose"
+import mongoose from "mongoose";
 
-const ProductSchema = new mongoose.Schema({
-    title: {
-        type: String,
-        required: true,
-        maxLenght: 60,
+const ProductSchema = new mongoose.Schema(
+    {
+        title: {
+            type: String,
+            required: true,
+            maxlength: 60,
+        },
+        desc: {
+            type: String,
+            required: true,
+            maxlength: 200,
+        },
+        img: {
+            type: String,
+            required: true,
+        },
+        prices: {
+            type: [Number],
+            required: true,
+        },
+        extraOptions: {
+            type: [
+                {
+                    text: { type: String, required: true },
+                    price: { type: Number, required: true },
+                },
+            ],
+        },
     },
-    desc: {
-        type: String,
-        required: true,
-        maxLenght: 200,
-    },
-    img: {
-        type: String,
-        required: true,
-    },
-    prices: {
-        type: [Number],
-        required: true,
-    },
-    extraOptions: {
-        type: [{ text: { type: String, required: true }, price: { type: Number, required: true } }],
-        required: true,
-        maxLenght: 60,
-    }
-}, { timestamps: true }
-)
+    { timestamps: true }
+);
 
-export default mongoose.model.Product || mongoose.model("Product", ProductSchema)
+export default mongoose.models.Product ||
+    mongoose.model("Product", ProductSchema);
