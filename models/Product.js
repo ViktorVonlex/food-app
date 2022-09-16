25222532b814218d5a96db02c922ed6e31a39aad
Jiří Šuster <suster.ji@gmail.com>
2022-09-16 20:49:15 +0200

    Get products API call

--- models/Product.js
+++ models/Product.js
@@ -25,9 +25,9 @@ const ProductSchema = new mongoose.Schema(
                 {
                     text: { type: String, required: true },
                     price: { type: Number, required: true },
-                },
-            ],
-        },
+                }
+            ]
+        }
     },
     { timestamps: true }
 );
