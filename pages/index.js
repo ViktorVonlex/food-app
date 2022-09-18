9314e75f07009a3e4bd520ad81ea6965a7e2c0f5
Jiří Šuster <suster.ji@gmail.com>
2022-09-18 12:02:06 +0200

    Using params from db to update frontend
    Still need to do cart

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,9 @@ import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import Featured from '../components/Featured'
 import PizzaList from '../components/PizzaList'
+import axios from 'axios'
 
-export default function Home() {
+export default function Home({pizzaList}) {
   return (
     <div className={styles.container}>
 
@@ -15,7 +16,16 @@ export default function Home() {
       </Head>
 
       <Featured />
-      <PizzaList/>
+      <PizzaList pizzaList={pizzaList}/>
     </div>
   )
+}
+
+export const getServerSideProps = async () => {
+  const res = await axios.get("http://localhost:3000/api/products")
+  return {
+    props: {
+      pizzaList: res.data
+    }
+  }
 }
