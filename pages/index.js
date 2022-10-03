6bd80c80ec361f864ffd0562543a1b7f7b8c98d7
Jiří Šuster <100037884+ViktorVonlex@users.noreply.github.com>
2022-10-03 10:02:03 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -7,13 +7,6 @@ import axios from 'axios'
 export default function Home({pizzaList}) {
   return (
     <div className={styles.container}>
-
-      <Head>
-        <title>Vonlex Pizza</title>
-        <meta name="description" content="Next best pizza chain." />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
       <Featured />
       <PizzaList pizzaList={pizzaList}/>
     </div>
