3d467d4bea0f4d97c6635e942de8a4cca334f43b
ViktorVonlex <suster.ji@gmail.com>
2022-10-03 15:26:39 +0200

    Added mantine carousel for featured

--- components/Featured.jsx
+++ components/Featured.jsx
@@ -1,18 +1,9 @@
 import React, { useState } from 'react'
 import Image from 'next/image'
 import styles from '../styles/Featured.module.css'
+import { Carousel } from '@mantine/carousel'
 
 function Featured() {
-    const [sliderIndex, setSliderIndex] = useState(0)
-
-    const handleArrow = (direction) => {
-        if(direction==='l'){
-            setSliderIndex(sliderIndex !== 0 ? sliderIndex-1 : 2)
-        }
-        if(direction==='r'){
-            setSliderIndex(sliderIndex !== 2 ? sliderIndex+1 : 0)
-        }
-    }
 
     const images = [
         "/img/featured1.png",
@@ -20,24 +11,17 @@ function Featured() {
         "/img/featured3.png"
     ];
 
-
-    return (
-        <div className={styles.container}>
-            <div className={styles.wrapper} style={{transform:`translateX(${-100*sliderIndex}vw)`}}>
-                {images.map((img, i) => (
-                    <div className={styles.imgContainer} key={i}>
+  return (
+    <div className={styles.container}>
+        <Carousel slideSize="70%" height="100%" sx={{ flex: 1 }} slideGap="md" controlsOffset="md" controlSize={28} loop withIndicators>
+            {images.map((img, i) => (
+                    <Carousel.Slide key={i}>
                         <Image src={img} alt="" layout='fill' objectFit='contain'/>
-                    </div>
-                ))}
-            </div>
-            <div className={styles.arrowContainer} style={{right:0}} onClick={()=>handleArrow("r")}>
-                <Image src="/img/arrowr.png" alt="" layout='fill' />
-            </div>
-            <div className={styles.arrowContainer} style={{left:0}} onClick={()=>handleArrow("l")}>
-                <Image src="/img/arrowl.png" alt="" layout='fill' />
-            </div>
-        </div>
-    )
+                    </Carousel.Slide>         
+            ))}
+        </Carousel>
+    </div>
+  );
 }
 
 export default Featured
