84581c2a70ce7054c752e858d80cf9e56a4f3e76
Jiří Šuster <suster.ji@gmail.com>
2022-10-03 10:00:09 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -8,12 +8,6 @@ export default function Home({pizzaList}) {
   return (
     <div className={styles.container}>
 
-      <Head>
-        <title>Vonlex Pizza</title>
-        <meta name="description" content="Next best pizza chain." />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
       <Featured />
       <PizzaList pizzaList={pizzaList}/>
     </div>
