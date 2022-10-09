bededba170a56aca19816a49835fefef47e85ac1
Jiří Šuster <suster.ji@gmail.com>
2022-10-09 21:10:52 +0200

    hm

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,8 @@ export default function Home({pizzaList}) {
 }
 
 export const getServerSideProps = async () => {
-  const res = await axios.get("http://localhost:3000/api/products")
+  const res = await axios.get("https://food-app-alpha-one.vercel.app/api/products")
+  console.log(res)
   return {
     props: {
       pizzaList: res.data
